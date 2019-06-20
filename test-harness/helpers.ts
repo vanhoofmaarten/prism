285e8a0d2abc58e83b33a47adef5f0dbe8b43cab
Vincenzo Chianese <vincenz.chianese@icloud.com>
2019-06-20 13:41:36 +0200

    feat: poc

--- test-harness/helpers.ts
+++ test-harness/helpers.ts
@@ -39,3 +39,12 @@ export function readFile(hash) {
 
   return JSON.parse(fileContent);
 }
+
+
+export function parseSpecFile(spec: string) {
+  const marker = '=====';
+
+  const [test, file, runCommand, clientCommand, expect] = spec.split(marker).map(t => t.trim());
+
+  return { test: test, file: file, runCommand: runCommand.split(' ').map(t => t.trim()), clientCommand: clientCommand, expect: expect }
+}
--- test-harness/index.ts
+++ test-harness/index.ts
@@ -1,6 +1,13 @@
 import { join } from 'path';
 import requests from './requests';
-import { makeRequest, constructMasterFileName, readFile } from './helpers';
+import { makeRequest, constructMasterFileName, readFile, parseSpecFile } from './helpers';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as tmp from 'tmp';
+import * as cp from 'child_process';
+import split2 = require('split2');
+
+jest.setTimeout(60000)
 
 async function runTest(req) {
   const { dynamic, ...request } = req;
@@ -18,7 +25,7 @@ async function runTest(req) {
 
 const spec = process.env.SPEC || join(__dirname, '/../examples/petstore.oas2.json');
 
-const createSpec = (specPath) => {
+const createSpec = specPath => {
   return () => {
     describe(specPath, () => {
       describe('When validating a supported server', () => {
@@ -273,4 +280,52 @@ const createSpec = (specPath) => {
   };
 };
 
-createSpec(spec)();
+// createSpec(spec)();
+
+describe('harness', () => {
+  const files = fs.readdirSync(path.join(__dirname, './specs/'));
+
+  files.forEach(value => {
+    const data = fs.readFileSync(path.join(__dirname, './specs/', value), { encoding: 'utf8' });
+    const parsed = parseSpecFile(data);
+
+    let prismMockProcessHandle: cp.ChildProcessWithoutNullStreams;
+    let tmpFileHandle: tmp.FileSyncObject;
+
+    beforeAll(done => {
+      tmpFileHandle = tmp.fileSync({
+        postfix: '.yml',
+        dir: undefined,
+        name: undefined,
+        prefix: parsed.test,
+        tries: 10,
+        unsafeCleanup: false,
+        template: undefined,
+      });
+
+      fs.writeFileSync(tmpFileHandle.name, parsed.file, { encoding: 'utf8' });
+      const args = [...parsed.runCommand, tmpFileHandle.name];
+
+      prismMockProcessHandle = cp.spawn(path.join(__dirname, '../cli-binaries/prism-cli-linux'), args);
+
+      prismMockProcessHandle.stdio.forEach(s => s.pipe(split2()).on('data', (s: string) => prismMockProcessHandle.connected && console.log(s)))
+      prismMockProcessHandle.stdout.pipe(split2()).on('data', (line: string) => {
+        if (line.includes('Prism is listening')) done();
+      });
+    });
+
+    afterAll(done => {
+      tmpFileHandle.removeCallback(null, null, null, null);
+      prismMockProcessHandle.kill();
+      prismMockProcessHandle.on('exit', done)
+
+    });
+
+    it(parsed.test, () => {
+      const [command, ...args] = parsed.clientCommand.split(' ');
+      const clientCommandHandle = cp.spawnSync(command, args, { encoding: 'utf8' });
+
+      expect(clientCommandHandle.stdout).toEqual(parsed.expect);
+    });
+  });
+});
